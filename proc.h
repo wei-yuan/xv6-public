0b20ffa28715346015b32fc85bbc65f029c7c52c
Weiyuan <ax61316@hotmail.com>
2017-12-17 20:16:36 +0800

    change proc_ticks from uint to int

--- proc.h
+++ proc.h
@@ -50,7 +50,7 @@ struct proc {
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
   // self defined variable
-  uint proc_ticks;
+  int proc_ticks;
 };
 
 // Process memory is laid out contiguously, low addresses first:
