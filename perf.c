de8cd7bc24e2f4812b50064cdd6f76ca0a1d480f
tsanming <tsanmmmm2000@hotmail.com>
2017-12-30 16:47:06 +0800

    Fix perf stat output calculation

--- perf.c
+++ perf.c
@@ -10,9 +10,9 @@ const char list_event_message[] = "List of pre-defined events:";
 const char usage_message[] = "usage: perf [FLAG] [COMMAND] [ARGS]";
 const char common_message[] = "The most commonly used perf commands are:";
 struct perfoutput help_output[] = {
-    { "list            ", "List all symbolic event types" },
-    { "stat            ", "Run a command and gather performance counter statistics" },
-    { "sched           ", "Tool to trace/measure scheduler properties (latencies)" }
+  { "list            ", "List all symbolic event types" },
+  { "stat            ", "Run a command and gather performance counter statistics" },
+  { "sched           ", "Tool to trace/measure scheduler properties (latencies)" }
 };
 struct perfoutput list_output[] = {
   { "cpu-migrations OR migrations               ", software_event },    
@@ -36,24 +36,67 @@ count_digit(int n)
 }
 
 void
-printfms(char *s, int n)
+printf_sperm(char *s, int n, int mticks)
+{
+  const char point[] = ".";
+  const char zero[] = "0";
+  char result[] = "\0";
+
+  // should check overflow
+  int mmticks = mticks * 1000;
+  int in = n/mmticks;
+  
+  if (n == 0){
+    printf(1, "  0  %s#      0 M/sec\n", s);
+  } else if (in > 0) {
+    printf(1, "  %d %s#      %d M/sec\n", n, s, in);            
+  } else {
+    int cn = n;
+    int digit = 1;
+    for(;;) {
+      cn *= 10;
+      digit++;
+      if ((cn/mticks)/1000 > 0) {
+        cn *= 10;
+        digit++;
+        break;
+      }
+    }
+    int zero_num = digit - 3;
+    int i;
+    strcat(result, point); 
+    for (i = 0; i < zero_num; i++) {
+      strcat(result, zero);    
+    }
+    printf(1, "  %d %s#      %d%s%d M/sec\n", n, s, 0, result, (cn/mticks)/1000);        
+  }
+}
+
+void
+printf_m2s(char* s, int m)
 {
   const char point[] = ".";
   const char zero[] = "0";
   char result[] = "\0";
   
-  if (n != 0) {
-    // one million has six zero digit
-    int zero_num = 6 - count_digit(n);
+  if (m%1000 > 0) {
+    int tm = m - ((m/1000) * 1000);
+    int zero_num = 3 - count_digit(tm);
     int i;
     strcat(result, point);        
     for (i = 0; i < zero_num; i++) {
       strcat(result, zero);    
     }
-    printf(1, "  %d %s#      %d%s%d M/sec\n", n, s, n/1000000, result, n);
+    printf(1, "\n %d%s%d %s\n\n", m/1000, result, tm, s);    
   } else {
-    printf(1, "  0  %s#      0 M/sec\n", s);
+    printf(1, "\n %d %s\n\n", m/1000, s);    
   }
+}
+
+void
+printf_cpus(char* s, int mcticks, int mtticks)
+{
+  printf(1, "  %d %s#      %d CPUs utilized\n", mcticks, s, mcticks/mtticks);
 }
 
 void
@@ -135,13 +178,20 @@ int main(int argc, char *argv[]){
         cmd->arg1 = pid;
         cmd->cmd = "end";
         perf_stat(cmd,ed);
+     
+        // ticks vs seconds?     
+        int mcputicks = ed->cputicks * 10;
+        int mtotalticks= ed->totalticks * 10;
+
+        // CPUs utilized = cpu-ticks msec / msec time elapsed
+        // M/sec = item / cpu-ticks msec / 1000
 
         printf(1, "\nPerformance counter stats for '%s':\n\n", cmd->test_cmd);
-        printfms("page-faults               ", ed->pgfault);
-        printfms("context-switches          ", ed->conswch);
-        printfms("cpu-ticks                 ", ed->cputicks);
-        printfms("cpu-migrations            ", ed->cpuswch);        
-        printf(1, "\n");
+        printf_sperm("    page-faults               ", ed->pgfault, mtotalticks);
+        printf_sperm("    context-switches          ", ed->conswch, mtotalticks);
+        printf_cpus("   cpu-ticks msec            ", mcputicks, mtotalticks);
+        printf_sperm("    cpu-migrations            ", ed->cpuswch, mtotalticks); 
+        printf_m2s("seconds time elapsed", mtotalticks);
     }
     exit();
 }
